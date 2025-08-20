4328c07ec294b3d5feafe658a4163f21f3a18038
BatLeDev <baptiste.guerny@koumoul.com>
2025-08-20 14:13:26 +0200

    feat: identities router update the created and updated name

--- api/src/misc/routers/identities.ts
+++ api/src/misc/routers/identities.ts
@@ -7,28 +7,59 @@ import mongo from '#mongo'
 
 const debug = Debug('webhooks-simple-directory')
 
+/** Helper function to update all collections */
+const updateAllCollections = async (filter: any, update: any) => {
+  await Promise.all([
+    mongo.catalogs.updateMany(filter, update),
+    mongo.imports.updateMany(filter, update),
+    mongo.publications.updateMany(filter, update)
+  ])
+}
+
+/** Helper function to delete from all collections */
+const deleteFromAllCollections = async (filter: any) => {
+  await Promise.all([
+    mongo.catalogs.deleteMany(filter),
+    mongo.imports.deleteMany(filter),
+    mongo.publications.deleteMany(filter)
+  ])
+}
+
 export default createIdentitiesRouter(
   config.secretKeys.identities,
   // onUpdate
   async (identity) => {
     debug('Incoming sd webhook for update', identity)
 
-    const filter = { 'owner.type': identity.type, 'owner.id': identity.id }
-    const update = { $set: { 'owner.name': identity.name } }
+    // Update owner name
+    await updateAllCollections(
+      { 'owner.type': identity.type, 'owner.id': identity.id },
+      { $set: { 'owner.name': identity.name } }
+    )
 
-    await mongo.catalogs.updateMany(filter, update)
-    await mongo.imports.updateMany(filter, update)
-    await mongo.publications.updateMany(filter, update)
+    if (identity.type) {
+      // Update created name
+      await Promise.all([
+        updateAllCollections(
+          { 'created.id': identity.id },
+          { $set: { 'created.name': identity.name } }
+        ),
+        updateAllCollections(
+          { 'updated.id': identity.id },
+          { $set: { 'updated.name': identity.name } }
+        )
+      ])
+    }
 
     if (identity.departments) {
-      for (const department of identity.departments) {
-        const filter = { 'owner.type': identity.type, 'owner.id': identity.id, 'owner.department': department.id }
-        const update = { $set: { 'owner.departmentName': department.name } }
-
-        await mongo.catalogs.updateMany(filter, update)
-        await mongo.imports.updateMany(filter, update)
-        await mongo.publications.updateMany(filter, update)
-      }
+      // Update department names
+      const departmentUpdates = identity.departments.map(department =>
+        updateAllCollections(
+          { 'owner.type': identity.type, 'owner.id': identity.id, 'owner.department': department.id },
+          { $set: { 'owner.departmentName': department.name } }
+        )
+      )
+      await Promise.all(departmentUpdates)
     }
   },
 
@@ -36,11 +67,7 @@ export default createIdentitiesRouter(
   async (identity) => {
     debug('Incoming sd webhook for delete', identity)
     // Delete all catalogs, imports and publications for this identity
-    const filter = { 'owner.type': identity.type, 'owner.id': identity.id }
-
-    await mongo.catalogs.deleteMany(filter)
-    await mongo.imports.deleteMany(filter)
-    await mongo.publications.deleteMany(filter)
+    await deleteFromAllCollections({ 'owner.type': identity.type, 'owner.id': identity.id })
 
     // Remote datasets are not deleted, only the links to them
     // When departments are deleted, do nothing
